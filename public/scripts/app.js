f559e35932963f42f2a9741a5f29f9adc6eddca7
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-09 16:52:02 -0700

    Add access token to map layer

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,5 +6,14 @@
   // .setView should have details provided by user - will have to update schema ([map center lat/lng], zoom)
   const userMap = L.map('.map').setView([51.505, -0.09], 13);
 
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoibmNtYWNsZWFuIiwiYSI6ImNsMXNpZTBkYzFvcXAzY21vdXZ6ZWNsdzMifQ.D-DWoaBxQjo5fNBZakdJsw}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibmNtYWNsZWFuIiwiYSI6ImNsMXNpZTBkYzFvcXAzY21vdXZ6ZWNsdzMifQ.D-DWoaBxQjo5fNBZakdJsw'
+}).addTo(userMap);
+
 
 });
