7e3bb785e7f0b99c0fea0521f2d481a53a865449
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-09 13:22:51 -0700

    Add profile.js

---
+++ routes/profile.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { user } = require('pg/lib/defaults');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/:id", (req, res) => {
+    const userID = req.sessions.id;
+    db.query(`SELECT * FROM maps WHERE active = true AND user_id = $1 ORDER BY created_on;`, [userID])
+      .then(data => {
+        const users = data.rows;
+        res.json({users});
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
