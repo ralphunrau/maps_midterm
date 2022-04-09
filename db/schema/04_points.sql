6ef095610200f82e2e14f0b140ace40ef3d7f2a7
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-09 16:45:11 -0700

    Add lat and lng to lat lng in db, due to req of leaflet

--- db/schema/04_points.sql
+++ db/schema/04_points.sql
@@ -4,8 +4,7 @@ CREATE TABLE points (
   id SERIAL PRIMARY KEY NOT NULL,
   map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
 
-  lat INT NOT NULL,
-  lng INT NOT NULL,
+  latlng VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
   description text,
   point_url VARCHAR(500) NOT NULL,
