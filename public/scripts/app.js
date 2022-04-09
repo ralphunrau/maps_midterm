1f4cad52f557363902e077fe6be4913e8b1ba97e
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-09 16:31:00 -0700

    Add leaflet as dependancy

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,8 @@
 // Client facing scripts here
+(() => {
+  // This will render the map
+  // .setView should have details provided by user - will have to update schema ([map center lat/lng], zoom)
+  const userMap = L.map('map').setView([51.505, -0.09], 13);
+
+
+});
