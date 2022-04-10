d6ac284263889e6d1130899e7451d9106b819d63
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-09 17:00:14 -0700

    Add temp marker function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,8 @@
   // .setView should have details provided by user - will have to update schema ([map center lat/lng], zoom)
   const userMap = L.map('.map').setView([51.505, -0.09], 13);
 
+  // This is what shows the road layer on the map
+
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoibmNtYWNsZWFuIiwiYSI6ImNsMXNpZTBkYzFvcXAzY21vdXZ6ZWNsdzMifQ.D-DWoaBxQjo5fNBZakdJsw}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -15,5 +17,12 @@
     accessToken: 'pk.eyJ1IjoibmNtYWNsZWFuIiwiYSI6ImNsMXNpZTBkYzFvcXAzY21vdXZ6ZWNsdzMifQ.D-DWoaBxQjo5fNBZakdJsw'
 }).addTo(userMap);
 
+  // This should add a marker - make into function that takes in points table data
 
+  const marker = L.marker([51.5, -0.09]).addTo(userMap);
+
+  const makeMarker = () =>{
+    const pointLatLng = //latlng info
+    L.marker([pointLatLng]).addTo(userMap);
+  }
 });
