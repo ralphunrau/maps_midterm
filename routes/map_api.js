e6cdd6b82f7e12bc8c7b8ce05c8d3ad4b3523644
RichardZhen123 <richardzhen5555@gmail.com>
2022-04-11 23:43:38 +0000

    minor update

--- routes/map_api.js
+++ routes/map_api.js
@@ -7,9 +7,9 @@ module.exports = (db) => {
   // RETURNS TABLE WITH SINGLE ROW OF ID IN URL
   router.get("/:id", (req, res) => {
     const mapID = req.params.id;
-    db.query(`SELECT * FROM maps WHERE active = true AND id = $1 ORDER BY created_on;`, [mapID])
+    db.query(`SELECT * FROM maps JOIN points ON maps.id = points.map_id WHERE maps.id = $1 AND maps.active = true AND points.active = true ORDER BY maps.created_on;`, [mapID])
       .then(data => {
-        const map = data.rows[0];
+        const map = data.rows;
         console.log(map);
         res.json(map);
       })
