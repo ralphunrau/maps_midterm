72ec0c8795ce20acb2c592eb575e683f857f5adc
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-12 12:19:13 -0700

    add cookies

--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.post("/", (req, res) => {
-    req.session.user_id = req.params.id;
+    req.session.userId = req.body.username;
     res.redirect('/profiles');
   });
   return router;
