d8320226dc4d51539a311f7bbe61638500a57aae
Ralph Unrau <xralphunrau@gmail.com>
2022-04-12 19:58:11 +0000

    Add form to marker popup and map titles to the right of map

--- routes/map_api.js
+++ routes/map_api.js
@@ -4,6 +4,19 @@ const router = express.Router();
 
 module.exports = (db) => {
 
+  router.get("/", (req,res) => {
+    db.query('SELECT * FROM maps;')
+      .then((data) => {
+        const map = data.rows;
+        res.json(map);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  })
+
   // RETURNS TABLE WITH SINGLE ROW OF ID IN URL
   router.get("/:id", (req, res) => {
     const mapID = req.params.id;
--- routes/maps.js
+++ routes/maps.js
@@ -64,7 +64,10 @@ module.exports = (db) => {
       });
   });
 
-
+  router.post("/add/:lat/:lng", (req, res) => {
+    console.log(req.params.lat, req.params.lng, req.body);
+    res.render('map_view')
+  })
 
 
 
