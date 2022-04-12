c567b5ab92cee4513560d2cf8555794c69ceb38e
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-11 17:26:46 -0700

    Add points to map git commit -m

--- public/scripts/map_view.js
+++ public/scripts/map_view.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
   $.get(`/api/maps/${id}`)
-  .then (data => {
-    // console.log (data)
-    $('#mapTitle').html(data.title);  
-    const map = L.map('map').setView([data.lng, data.lat], 13);
-    L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`).addTo(map);
+    .then(data => {
+      $('#mapTitle').html(data[0].map_title);
+      const map = L.map('map').setView([data[0].map_lng, data[0].map_lat], 13);
+      L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`).addTo(map);
 
-    L.marker([data.lng, data.lat]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
-
-    const popup = L.popup();
-
-    function onMapClick(e) {
-      popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-    }
-  });
+      for (const point of data) {
+        let lat = point.point_lat;
+        let lng = point.point_lng;
+        L.marker([lng, lat]).addTo(map)
+          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+          .openPopup();
+        }
+        const popup = L.popup();
+      function onMapClick(e) {
+        popup
+          .setLatLng(e.latlng)
+          .setContent("You clicked the map at " + e.latlng.toString())
+          .openOn(map);
+      }
+    });
 });
--- routes/map_api.js
+++ routes/map_api.js
@@ -7,12 +7,18 @@ module.exports = (db) => {
   // RETURNS TABLE WITH SINGLE ROW OF ID IN URL
   router.get("/:id", (req, res) => {
     const mapID = req.params.id;
-    db.query(`SELECT * JOIN points ON maps.id = points.map_id
-    WHERE maps.id = $1 AND maps.active = true AND points.active = true ORDER BY maps.created_on;`, [mapID])
+    db.query(`SELECT *
+    FROM maps
+    JOIN points
+    ON maps.id = points.map_id
+    WHERE maps.id = $1
+    AND maps.map_active = true
+    AND points.point_active = true
+    ORDER BY map_created_on;`, [mapID])
       .then(data => {
 
         const map = data.rows;
-        console.log(map);
+
         res.json(map);
       })
       .catch(err => {
