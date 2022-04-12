f720f20b5a8c805b3ad60079aef8be47e0076a83
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-12 15:51:08 -0700

    point desc

--- routes/maps.js
+++ routes/maps.js
@@ -16,19 +16,11 @@ module.exports = (db) => {
     res.render('map_create');
   });
 
-<<<<<<< HEAD
-  // RETURNS TABLE WITH SINGLE ROW OF ID IN URL
-  router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    res.render('map_view', {id});
-  });
-=======
  // RETURNS TABLE WITH SINGLE ROW OF ID IN URL
   router.get("/:id", (req, res) => {
     const id = req.params.id;
     res.render('map_view', {id});
 });
->>>>>>> master
 
   //Updates points table with new point
   router.post("/add/:lat/:lng/:id", (req, res) => {
