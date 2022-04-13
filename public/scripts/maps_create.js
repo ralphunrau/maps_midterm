18655a3a6f3c5249072c278962cffec10c24044c
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-13 13:24:48 -0700

    Tackle create mamps problem

--- public/scripts/maps_create.js
+++ public/scripts/maps_create.js
@@ -1,26 +1,22 @@
-$(document).ready(function() {
-  const map = L.map('map').setView([49.286796, -123.129427],60);
+$(document).ready(function () {
+  const map = L.map('map').setView([49.286796, -123.129427], 60);
   L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`).addTo(map);
 
   const popup = L.popup();
-  const onMapClick = (e) => {
-    L.marker([e.latlng.lat, e.latlng.lng])
-      .bindPopup('A point of interest.')
-      .openPopup();
+  function onMapClick(e) {
     popup
       .setLatLng(e.latlng)
       .setContent(`You clicked the map at ${e.latlng.lat}, ${e.latlng.lng}. Give this point some information:`)
       .openOn(map);
     const popupForm = $(`
-      <form class ='pointCreationForm' action='/maps/add/${e.latlng.lng}/${e.latlng.lat}/${data[0].id}' method='POST'>
-        <div><textarea name='title' placeholder ='Enter a title:' style='height: 20px;'></textarea></div>
-        <div><textarea name='description' placeholder ='Enter a description:' style='height: 40px;'></textarea></div>
-        <div><textarea name='image' placeholder ='Enter a image url:' style='height: 20px;'></textarea></div>
-        <button class='submit'>Sumbit</button>
-      </form>
-    `);
-    $('.leaflet-popup-content').append(popupForm);
-  };
-  map.on('click', onMapClick);
+    <form class ='pointCreationForm' action='/maps/create/${e.latlng.lat}/${e.latlng.lng}' method='POST'>
+      <div><textarea name='title' placeholder ='Enter a title:' style='height: 20px;'></textarea></div>
+      <div><textarea name='description' placeholder ='Enter a description:' style='height: 40px;'></textarea></div>
+      <div><textarea name='image' placeholder ='Enter a image url:' style='height: 20px;'></textarea></div>
+      <button class='submit'>Sumbit</button>
+    </form>
+  `);
+    $('.leaflet-popup-content').append(popupForm)
+  }
 
 });
--- routes/maps.js
+++ routes/maps.js
@@ -38,13 +38,14 @@ module.exports = (db) => {
     res.render('map_view', {id});
   });
 
-  router.post("/create", (req, res) => {
+
+  router.post("/create/:lat/:lng", (req, res) => {
     const id = req.session.userId;
     const title = req.body.title;
     const description = req.body.description;
     const image = req.body.image;
-    const lng = 49.28249;
-    const lat = -123.127576;
+    const lng = req.params.lng;
+    const lat = req.params.lat;
     db.query('INSERT INTO maps (user_id, map_lng, map_lat, map_title, map_pic_url,map_description) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *',
       [id, lng, lat, title, description, image])
       .then(data => {
