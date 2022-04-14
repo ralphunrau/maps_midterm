f73041c917694f3526650e659dcfc9a6ceea8c7c
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-14 11:24:29 -0700

    Add reroute if not user add point

--- routes/maps.js
+++ routes/maps.js
@@ -9,41 +9,45 @@ module.exports = (db) => {
   // RETURNS TABLE OF ALL MAPS
   router.get("/", (req, res) => {
     const userId = req.session.userId;
-    res.render('index', {userId});
+    res.render('index', { userId });
   });
 
   // RETURNS TABLE WITH SINGLE ROW OF ID IN URL
   router.get("/create", (req, res) => {
     const userId = req.session.userId;
-    res.render('map_create', {userId});
+    res.render('map_create', { userId });
   });
 
   // RETURNS TABLE WITH SINGLE ROW OF ID IN URL
   router.get("/:id", (req, res) => {
     const id = req.params.id;
     const userId = req.session.userId;
-    res.render('map_view', {id, userId});
+    res.render('map_view', { id, userId });
   });
 
   //Updates points table with new point
   router.post("/add/:lat/:lng/:id", (req, res) => {
     const userId = req.session.userId;
-    db.query('SELECT user_id FROM maps WHERE maps.id = $1', [req.params.id])
-      .then((data) => {
-        if ((data.rows[0].user_id) !== userId) {
-          // window.alert("Unable to add points to other users maps.");
-
-        }
-      });
+    let id = req.params.id;
     let lat = req.params.lat;
     let lng = req.params.lng;
-    let id = req.params.id;
     let title = req.body.title;
     let description = req.body.description;
     let image = req.body.image;
-    db.query('INSERT INTO points (map_id,point_lng, point_lat, point_title, point_description, point_url) VALUES ($1,$2,$3,$4,$5,$6)',
-      [id, lng, lat, title, description, image]);
-    res.render('map_view', {id, userId});
+
+    db.query('SELECT user_id FROM maps WHERE maps.id = $1', [req.params.id])
+      .then((data) => {
+        if ((data.rows[0].user_id) !== userId) {
+          console.log(">>>>>>>id no match<<<<<<<", data.rows[0].user_id, userId);
+          res.render('map_view', { id, userId });
+          return;
+        } else {
+          db.query('INSERT INTO points (map_id,point_lng, point_lat, point_title, point_description, point_url) VALUES ($1,$2,$3,$4,$5,$6)',
+            [id, lng, lat, title, description, image]);
+          res.render('map_view', { id, userId });
+          console.log("second promise");
+        }
+      });
   });
   //CREATES NEW MAP
   router.post("/create/:lat/:lng", (req, res) => {
@@ -56,21 +60,21 @@ module.exports = (db) => {
     const pointImage = req.body.pointImage;
     const lat = req.params.lng;
     const lng = req.params.lat;
-    db.query('INSERT INTO maps (user_id, map_lng, map_lat, map_title, map_description, map_pic_url) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *',[userId, lng, lat, mapTitle, mapDescription, mapImage])
-      .then(data=>{
+    db.query('INSERT INTO maps (user_id, map_lng, map_lat, map_title, map_description, map_pic_url) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *', [userId, lng, lat, mapTitle, mapDescription, mapImage])
+      .then(data => {
         const id = data.rows[0].id;
         db.query(`INSERT INTO points (map_id,point_lng, point_lat, point_title, point_description, point_url) VALUES ($1, $2, $3, $4, $5, $6)`, [id, lng, lat, pointTitle, pointDescription, pointImage]);
-        res.render('map_view', {id, userId});
+        res.render('map_view', { id, userId });
       });
   });
   // DELETE SELECTED POINT FROM MAP
 
-  router.post("/:id/:point_title/delete", (req, res) =>{
+  router.post("/:id/:point_title/delete", (req, res) => {
     const userId = req.session.userId;
     const id = req.params.id;
     const titleId = req.params.point_title;
     db.query(`UPDATE points SET point_active = false WHERE point_title = $1`, [titleId]);
-    res.render("map_view", {id, userId});
+    res.render("map_view", { id, userId });
   });
   //CHANGE POINT TO NOT ACTIVE - TO DELETE FROM MAP
   router.get("/:id/:map_title/delete", (req, res) => {
@@ -78,7 +82,7 @@ module.exports = (db) => {
     const id = req.params.id;
     const mapTitle = req.params.map_title;
     db.query(`UPDATE points SET point_active = false WHERE point_title = $1`, [mapTitle]);
-    res.render('map_view', {id, userId});
+    res.render('map_view', { id, userId });
   });
   //EDITS SELECTED VALUES IN ROW ON POINT TABLE
   router.post("/:id/:point/edit", (req, res) => {
@@ -108,7 +112,7 @@ module.exports = (db) => {
       .then(data => {
         const userId = req.session.userId;
         const maps = data.rows;
-        res.render("map_view", {id, userId});
+        res.render("map_view", { id, userId });
       })
       .catch(err => {
         console.log("error in catch");
